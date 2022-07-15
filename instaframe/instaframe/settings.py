646c0320198073b03aa0ba79f28dd0edfe16feee
Mau <dreg5000.bbb@gmail.com>
2022-07-15 15:19:06 -0400

    static files

--- instaframe/instaframe/settings.py
+++ instaframe/instaframe/settings.py
@@ -162,4 +162,4 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-STATICFILES_STORAGE = "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
