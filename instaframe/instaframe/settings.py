1a1a47206e29b711cd75c24a046df488cc29843e
Mau <dreg5000.bbb@gmail.com>
2022-07-15 15:12:22 -0400

    Django.contrib instead whitenoise

--- instaframe/instaframe/settings.py
+++ instaframe/instaframe/settings.py
@@ -162,4 +162,4 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
