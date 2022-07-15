90e3a7e23f75bb5d94d9d11681ffee99d2667713
Mau <dreg5000.bbb@gmail.com>
2022-07-15 13:08:55 -0400

    Revert "KEEPING SECRETS SECRET"
    This reverts commit 9459ff9684e4561f30dc4dddf7478cca08b0467c.

--- instaframe/instaframe/settings.py
+++ instaframe/instaframe/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = 'django-insecure-wdzch$x^d^2cte+w^jjh1%1g#$zo^4@tym3_f)*g-ssm71t083'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
